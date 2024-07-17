752069105622df6b25ccfe508bd7772ca9cacc62
taiwosugar <oguntoyinborichman@gmail.com>
2024-07-17 22:45:02 +0100

    dd

--- src/App.js
+++ src/App.js
@@ -7,12 +7,21 @@ import Login from './pages/authentication/Login';
 import Room from './pages/room/Room';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
+import { FaPhone, FaWhatsapp } from 'react-icons/fa';
 
 
 const App = () => {
   return (
     <Router>
       <Navbar/>
+      <a href='tel: +1-614-707-6245'>
+                 <div className='side-box'>
+                    <h6><FaPhone/><br/> Diamond</h6>
+                </div> </a>
+                <a href='https://wa.me/message/NL7UY3M3Q6ZOG1'> 
+                  <div className='side-box2'>
+                    <h6><FaWhatsapp/><br/>Book Now</h6> 
+                </div></a>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
