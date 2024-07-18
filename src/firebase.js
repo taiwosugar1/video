e3ce9bde9e72ad2dcc779e704d28c371a2498230
taiwosugar <oguntoyinborichman@gmail.com>
2024-07-18 14:22:56 +0100

    ffgg

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { REACT_APP_API_KEY, REACT_APP_APP_ID, REACT_APP_AUTH_DOMAIN, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET } from "./config";
+
 
 const firebaseConfig = {
-    apiKey: REACT_APP_API_KEY,
-    authDomain: REACT_APP_AUTH_DOMAIN,
-    projectId: REACT_APP_PROJECT_ID,
-    storageBucket:  REACT_APP_STORAGE_BUCKET,
-    messagingSenderId:  REACT_APP_MESSAGING_SENDER_ID,
-    appId: REACT_APP_APP_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
+    projectId:   process.env.REACT_APP_PROJECT_ID,
+    storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId:  process.env.REACT_APP_APP_ID
   };
   
 
